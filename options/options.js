e61c8ad4ad59b4cbd13d289614dec97c7c70c794
Crowdbotics <team@crowdbotics.com>
2021-10-12 20:42:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "young_glitter_31360",
+  url: "https://young_glitter_31360.botics.co",
+  api: "https://young_glitter_31360.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
